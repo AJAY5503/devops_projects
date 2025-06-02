28de7888b30f37abba2e06afba59c89a8955800b
Venkata Ajay Kandregula <jaykan2277@gmail.com>
2025-06-02 15:30:24 +0530

    sysinfo modified

--- shellscripts/sysinfo.sh
+++ shellscripts/sysinfo.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-echo "device name "
-uname
-echo -e "\n hostname:"
-uname -n
-echo -e "\n how long the system is up"
-uptime
-echo -e "\n free space "
-free
+#author : ajay
+
+echo "===================system info===================="
+echo -e "\n device name $(uname -o)"
+echo -e "\n hostname : $(hostname) "
+echo -e "\n uptime : $(uptime -p)"
+echo -e "\n memory used:"
+free -h | grep Mem | awk '{print $3 " used out of " $2}'
+echo -e "\n disk usage "
+df -h | grep dev | awk '{print $3 "used out of" $4 }'
